a8b4d5586efc037e95d17194621c9995825c4c17
A.Hassanpour <77358339+AHassanpour88@users.noreply.github.com>
2022-08-26 14:47:29 +0200

    Update Sensivity_analysis.R

--- Sensivity_analysis.R
+++ Sensivity_analysis.R
@@ -5,31 +5,25 @@ load("results_full.RData")
 
 # sensitivity -------------------------------------------------------------
 
-sensivity_analysis <- function(data
-                       , sample_size 
-                       , rep 
+simulation <- function(data
+                       , sample_size
+                       , rep
                        , n_bull = seq(100, 700, by = 1)
                        , n_sel = seq(6, 30, by = 1)
                        ) {
 
 sim_n <- function(data, sample_size, rep) {
-  # loop with the name of the samples
+  
   1:length(sample_size) %>% 
-    # adding the name of the list
     purrr::set_names(sample_size) %>% 
-    # map is sorta equal to loop, it apply a function to each element of a vector or list
-    # map always returns a list
     map(function(x) {
       
-      # access the size of the samples
       size <- sample_size[x] %>% as.numeric()
       
-      ## do the sampling for number of repetitions
       1:rep %>% 
         map( function(y) {
           
-          # sampling from initial 60000 simulations depends on the sample size
-          data[sample(nrow(data), size = size, replace=FALSE),] 
+          data[sample(nrow(data), size = size, replace=TRUE),] 
           
         }) 
       
@@ -37,23 +31,23 @@ sim_n <- function(data, sample_size, rep) {
   
 } 
 
-# use the function for smoothing procedure, with the same name to don't redo it, it can have different length 1000,20000
+# data = results
+# rep = 5
+# sample_size = c(100, 200)
+
 mysim <- sim_n(data
                , sample_size 
                , rep
                )
 
 allsim <- 1:length(mysim) %>% 
-  # add the names again
   purrr::set_names(sample_size) %>% 
   map(function(x) {
     
-    ## extracting the list by name
     results <- mysim[[names(mysim)[[x]]]]
     
     1:rep %>% map(function(p) {
       
-      #smoothing function
       approx_f <- function(x1, x2, x3, bw=1){
         dist <- sqrt((results[[p]][,2] - x3)^2+ (results[[p]][,3] - x1)^2 + (30*(results[[p]][,4] -x2))^2 )
         weigth <- dnorm(dist, sd=30*bw)
@@ -64,9 +58,8 @@ allsim <- 1:length(mysim) %>%
         return (both)
       }
       
-      rel1 <- rel2 <-  ir_sim <- matrix(0, nrow=length(n_bull), ncol=length(n_sel)) 
+      ir <- rel <- rel1 <- rel2 <-  ir_sim <- matrix(0, nrow=length(n_bull), ncol=length(n_sel)) 
       
-      #apply smoothing for n_bull and n_sel in search space
       for(index in 1:length(n_bull)){
         print(index)
         for(index2 in 1:length(n_sel)){
@@ -91,63 +84,25 @@ allsim <- 1:length(mysim) %>%
       )
       
     }) %>% 
-      #join the repetitions by scenario
       bind_rows()
     
   }) 
 
 }
 
-rsl <- sensivity_analysis(data = results
+rsl_repl <- simulation(data = results
                   , rep = 50
                   , sample_size = c(1000, 10000,20000,30000,40000,50000,60000)
-                  , n_bull = 
                   )
 
-save(rsl, file = "Results_50rep.Rdata")
+save(rsl_repl, file = "Results_50rep_withreplacement.Rdata")
 
-rsl %>% 
-  #make it as a table
+rsl_repl %>% 
   bind_rows() %>% 
   mutate(across(scenario, as.factor)) %>% 
   ggplot(aes(x = scenario, y = target, group = scenario, fill = scenario)) +
   geom_boxplot() +
   theme_classic()
 
-rsl %>% 
-  #make it as a table
-  bind_rows() %>% 
-  mutate(across(scenario, as.factor)) %>% 
-ggplot(aes(x = ordered(scenario),
-           y = abs(target-107.8),
-           color=scenario,
-           group = scenario))+
-  geom_violin(scale = "width")+
-  geom_boxplot(width = 0.2)+
-  geom_point(position = position_jitter(width = 0.2,height = 0))+ 
-  labs(x = "Scenarios",
-       y = "Target") +
-  theme_classic()
 
 
-
-rsl %>% 
-  #make it as a table
-  bind_rows() %>% 
-  mutate(across(scenario, as.factor)) %>% 
-  ggplot(aes(x = ordered(scenario),
-             y = abs(n_bull),
-             color=scenario,
-             group = scenario))+
-  geom_violin(scale = "width")+
-  geom_boxplot(width = 0.2)+
-  geom_hline(yintercept = 224)+
-  geom_point(position = position_jitter(width = 0.2,height = 0))+
-  theme_classic()+ 
-  labs(x = "Scenarios",
-       y = "Number of Bulls") +
-  theme(axis.text = element_text(size = 12),
-        panel.border = element_rect(color = "black",
-                                    fill = NA,
-                                    size = 1.2))  +
-  scale_y_continuous(breaks=seq(100,700,by=100))
